98337a4b94b4d8fef6904cb2784d63a81ce4a198
Afzal Ansari <afzal442@gmail.com>
2024-11-23 23:18:13 +0000

    rmvs unwanted apis

--- examples/gofr-bot/main.go
+++ examples/gofr-bot/main.go
@@ -9,13 +9,10 @@ func main() {
 	// Create a new GoFr application
 	app := gofr.New()
 
-	// app.AddHTTPService("anotherService", "http://localhost:9000")
-
 	// Define routes
 	app.GET("/api/social/posts", handlers.GeneratePostHandler)
 	app.POST("/api/social/approve", handlers.ApprovePostHandler)
 	app.POST("/api/email/outreach", handlers.EmailOutreachHandler)
-	// app.GET("api/analytics", handlers.AnalyticsHandler)
 
 	// Run the application
 	app.Run()
